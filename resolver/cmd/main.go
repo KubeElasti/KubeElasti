5a1b22c7a9fde73cec9b4bd9c6fc8663810651c4
Raman Tehlan <ramantehlan@gmail.com>
2026-04-02 19:46:10 +0530

    resolve conflict

--- resolver/cmd/main.go
+++ resolver/cmd/main.go
@@ -149,7 +149,12 @@ func main() {
 	internalServeMux := http.NewServeMux()
 	internalServeMux.Handle("/metrics", promhttp.Handler())
 	internalServeMux.Handle("/queue-status", sentryHandler.HandleFunc(requestHandler.GetQueueStatus))
+<<<<<<< HEAD
 	internalServeMux.Handle("/crd-cache-status", sentryHandler.HandleFunc(requestHandler.GetCRDCacheStatus))
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 14b2891 (feat: add main)
 	internalServeMux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, err := w.Write([]byte("ok"))
@@ -166,6 +171,7 @@ func main() {
 			return
 		}
 	})
+
 	internalServer := &http.Server{
 		Addr:              internalPort,
 		Handler:           internalServeMux,
